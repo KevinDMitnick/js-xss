252fc259079460d902a61262dacaee67e3e51378
lizichao <lizichao@smartisan.com>
2018-09-18 16:42:08 +0800

    add comment

--- dist/xss.js
+++ dist/xss.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 /**
  * default settings
  *
@@ -727,9 +727,9 @@ module.exports = {
     return str.replace(/(^\s*)|(\s*$)/g, "");
   },
   spaceIndex: function(str) {
-    var reg = /\s|\n|\t/;
-    var match = reg.exec(str);
-    return match ? match.index : -1;
+    var reg = /\s|\n|\t/;    // 创建一个正则表达式，匹配 空格、换行、制表；
+    var match = reg.exec(str);    // 返回 str 中第一个匹配 reg 的字符
+    return match ? match.index : -1;    // 返回匹配的字符的位置索引
   }
 };
 
